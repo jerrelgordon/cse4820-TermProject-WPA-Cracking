40baea02b69a4ed0153633f9fdd142af6081d676
jerrelgordon <73038486+jerrelgordon@users.noreply.github.com>
2022-12-01 13:29:45 -0500

    Add files via upload

--- cleanup.py
+++ cleanup.py
@@ -3,4 +3,7 @@ import os
 
 os.system("rm firstdump.txt")
 os.system("rm airodump1.txt")
-os.system("output-o1.cap")
+os.system("rm output-01.cap")
+os.system("rm output-01.csv")
+os.system("rm output-01.kismet.csv")
+os.system("rm output-01.log.csv")
